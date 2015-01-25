64123abc2520511a202b28de6c8ebca2e8025cfe
hdg <hdg1988@gmail.com>
2015-01-25 23:46:06 +0800

    fix

---
+++ views/common/ins.php
@@ -0,0 +1,25 @@
+<?php
+
+function getLang(){
+	$lang = isset( $_COOKIE['lang'] ) ? $_COOKIE['lang'] : '';
+	if(  empty( $lang ) ){
+		$lang = 'eng';
+	}
+	return $lang;
+}
+
+function getStrings(){
+	static $strings;
+	if( $strings ){
+		return $strings;
+	}
+	$lang = getLang();
+
+	$strings = require_once ROOT . '/views/langs/' . $lang . '.php';
+	return $strings;
+}
+
+function _e( $str ){
+	$strings = getStrings();
+	echo !isset( $strings[ $str ] ) || empty( $strings[ $str ] ) ? $str : $strings[ $str ];
+}
