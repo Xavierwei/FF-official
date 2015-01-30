e284b9cc5dda1ff30976fd31e2784a0cd98d5f80
hdg <hdg1988@gmail.com>
2015-01-30 23:36:00 +0800

    fix

--- views/contact.php
+++ views/contact.php
@@ -7,10 +7,11 @@
   <div class="section sec_contact column">
     <!--  -->
     <h2 class="contact_tit">
-			<?php _e('Fred & Farid is a digital creative agency<br /> based in shanghai and paris'); ?>
-		</h2>
+		<?php _e('Fred & Farid is a digital creative agency<br /> based in shanghai and paris'); ?>
+	</h2>
     <!--  -->
     <div class="contact_item"></div>
+  </div>
   <div data-effect="fadeup" class="intoview-effect contact_map" id="map">
   </div>
   <?php include_once 'common/footer.php';?>
