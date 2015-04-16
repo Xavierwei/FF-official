c6ed5200f608102b8376b3f04fbcc6fb5dde825c
Frank.Zhang <shengbeiniao@qq.com>
2015-04-16 16:28:08 +0800

    fix bio 样式

--- js/lp.base.js
+++ js/lp.base.js
@@ -6625,6 +6625,7 @@ $(function(){
         if(window.location.href.indexOf('bio')>0){
             $('.banpho-bt a').hide();
             $('.banpho-i').hide();
+            $('.showreel-tit').hide();
         }
     }
     bio_hide();
