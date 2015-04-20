345b47c644af2830d763ac01b62a7036fe4431ff
Frank.Zhang <shengbeiniao@qq.com>
2015-04-20 10:59:53 +0800

    2. Arrow - please make perfect alignment top: 33px; not top: 30 px; on .pagetitarr 4. BIO - remove "," after "SIR JOHN HEGARTY LES ECHOS" 5. Job filter order must be PA SH BE NY

--- views/jobs.php
+++ views/jobs.php
@@ -11,9 +11,9 @@
     <h2 class="job_tit">
         <?php _e('JOB TITLE'); ?>
         <ul class="f-c cs-clear" >
+            <li><input type="checkbox" id="paris" data-city="PARIS" /><label data-a="jobFilter" for="paris">Paris</label></li>
             <li><input type="checkbox" id="shanghai" data-city="SHANGHAI" /><label data-a="jobFilter" for="shanghai">Shanghai</label></li>
             <li><input type="checkbox" id="beijing" data-city="BEIJING"/><label data-a="jobFilter"  for="beijing">Beijing</label></li>
-            <li><input type="checkbox" id="paris" data-city="PARIS" /><label data-a="jobFilter" for="paris">Paris</label></li>
             <li><input type="checkbox" id="new_york" data-city="NEW YORK" /><label data-a="jobFilter" for="new_york">New York</label></li>
         </ul>
     </h2>
