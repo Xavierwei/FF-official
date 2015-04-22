6d24c768ccfe79b0ff2ee575f179d94b3c2c9ff7
Jackey <jackey@fumer.cn>
2015-04-22 18:23:20 +0800

    .

--- js/lp.base.js
+++ js/lp.base.js
@@ -30,7 +30,8 @@ LP.use(['/js/plugin/jquery.easing.1.3.js', '../api'], function (easing, api) {
     var initSoundWave = function( file, $canves , cb ){
         //file = 'http://ff.local/audios/20140419_chine_0_2.mp3';
         //file = 'http://ff.local/audios/20140419_chine_0_2_1.mp3';
-        file = file.replace('http://cdn.fredfarid.com', '');
+        file = 'http://cdn.fredfarid.com/data/eng/about/interviews/radio//media/lemouv_-_re_e_couter_-_mardi_16_juillet_10-03-45.mp3';
+        file = file.replace('http://cdn.fredfarid.com', 'http://fredfarid.com');
         LP.use('waveform', function(){
 
           var
