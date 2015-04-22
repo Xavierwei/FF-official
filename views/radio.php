e68c27d2cd7ffe5e634a2075694bf94e856854a8
Jackey <jackey@fumer.cn>
2015-04-22 15:02:49 +0800

    interview -> tv/radio

---
+++ views/radio.php
@@ -0,0 +1,22 @@
+<?php include_once 'common/header.php';?>
+<div class="page" data-page="audio-page" data-header="press">
+	<!-- page title -->
+	<?php 
+	$top_title = _e('PRESS',false);
+	$pagetit = _e('AUDIO',false);
+	include_once 'common/pagetit.php';?>
+
+
+	<!-- section -->
+	<div class="section sec_press">
+		<div>
+			<!--  -->
+			<div class="press_txt">
+				<?php _e('FRED &amp; FARID\'s interviews'); ?>
+			</div>
+			<!--  -->
+			<div id="press-container" class="interview_list cs-clear">
+			</div>
+		</div>
+	</div>
+<?php include_once 'common/footer.php';?>
