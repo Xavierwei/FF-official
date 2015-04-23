1ae7bf77aaf2f43f2a15f62abc3be321ce5e1d99
Jackey <jackey@fumer.cn>
2015-04-23 15:23:20 +0800

    debug weibolist

--- api/weibo/weibolist.php
+++ api/weibo/weibolist.php
@@ -1,23 +1,48 @@
 <?php
-    // 每天 早上9点重新抓一次
-    $cacheFile = __DIR__.'/cache.txt';
-    $output = file_get_contents($cacheFile);
 
-    if (date('G') == 9 || $output == '') {
-        include_once( 'config.php' );
-        include_once( 'saetv2.ex.class.php');
-        $token = file_get_contents("token.txt");
-        $c = new SaeTClientV2( WB_AKEY , WB_SKEY , $token );
-        $ms  = $c->user_timeline_by_id(WB_UID,1,2);
-        $user = $c->show_user_by_id(WB_UID);
-        $ms['userinfo'] = $user;
-
-        $ret = json_encode($ms);
-
-        file_put_contents($cacheFile, $ret);
-        $output = $ret;
+function debugEcho() {
+    $args = func_get_args();
+    $str = '';
+    foreach ($args as $arg) {
+        $str .= print_r($arg, true)."\n";
     }
 
+    $file = fopen(__DIR__.'/debug.txt', 'a+');
+    if ($file) {
+        fwrite($file, $str);
+    }
 
-    echo $output;
+    fclose($file);
+
+    return true;
+}
+
+// 每天 早上9点重新抓一次
+$cacheFile = __DIR__.'/cache.txt';
+$output = file_get_contents($cacheFile);
+
+debugEcho('====== begin fetch weibo ========');
+debugEcho('=====  now is '. date('G').' =======');
+
+if (date('G') == 9 || $output == '') {
+    debugEcho('===== fetching weibo ======= ');
+    include_once( 'config.php' );
+    include_once( 'saetv2.ex.class.php');
+    $token = file_get_contents("token.txt");
+    $c = new SaeTClientV2( WB_AKEY , WB_SKEY , $token );
+    $ms  = $c->user_timeline_by_id(WB_UID,1,2);
+    $user = $c->show_user_by_id(WB_UID);
+    $ms['userinfo'] = $user;
+
+    $ret = json_encode($ms);
+
+    file_put_contents($cacheFile, $ret);
+    $output = $ret;
+}
+
+debugEcho('=======end fetch weibo ========');
+
+
+
+echo $output;
 ?>
